0b02c83c82fa9b215666bf59b40ed7a9f7cf1abe
Kreason Aaron Naidoo <kreasonaaron@me.com>
2016-04-14 09:16:45 +0200

    Rewrote the function to be more general and made spelling corrections

--- Tutorials/Tutorial 2.py
+++ Tutorials/Tutorial 2.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Question 1
 
 print
@@ -8,7 +7,7 @@ print "Question 1 has no output"
 print
 print
 
-
+'''used in old method
 x1 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/10) # x is a range of numbers from 0 to half pi, 10 points
 x2 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/30) # x is a range of numbers from 0 to half pi, 30 points
 x3 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/100) # x is a range of numbers from 0 to half pi, 100 points
@@ -19,7 +18,7 @@ x6 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/1000000) # x is a range of numbers fr
 
 # Question 2
 
-def intergrate (ran):
+def integrate_old (ran):
 
     width = ran[1] - ran[2]
 
@@ -30,32 +29,51 @@ def intergrate (ran):
 
     return sum
 
+'''
+
+def integrate(low, high, points, function):
+
+    step = (high - low) / points
+
+    x = np.arange(low, high, step)
+
+
+    sum = 0
+
+    for i in range(len(x)):
+        sum = sum + (function(x[i])) * step
+
+    return sum
+
+
 
 
 print "Question 2"
 print
 print
 
-print "The actual value of the intergral of cos(x) between 0 and pi/2 is -1"
+print "The actual value of the integral of cos(x) between 0 and pi/2 is 1"
 print
 print
 
-print "The value of the intergral of cos(x) between 0 and pi/2 for 10 points is: ", intergrate(x1)
+print "The value of the integral of cos(x) between 0 and pi/2 for 10 points is: ", integrate(0, 0.5 * (np.pi), 10, np.cos)
 print
 
-print "The value of the intergral of cos(x) between 0 and pi/2 for 30 points is: ", intergrate(x2)
+print "The value of the integral of cos(x) between 0 and pi/2 for 30 points is: ", integrate(0, 0.5 * (np.pi), 30, np.cos)
 print
 
-print "The value of the intergral of cos(x) between 0 and pi/2 for 100 points is: ", intergrate(x3)
+print "The value of the integrall of cos(x) between 0 and pi/2 for 100 points is: ", integrate(0, 0.5 * (np.pi), 100, np.cos)
 print
 
-print "The value of the intergral of cos(x) between 0 and pi/2 for 300 points is: ", intergrate(x4)
+print "The value of the integrall of cos(x) between 0 and pi/2 for 300 points is: ", integrate(0, 0.5 * (np.pi), 300, np.cos)
 print
 
-print "The value of the intergral of cos(x) between 0 and pi/2 for 1000 points is: ", intergrate(x5)
+print "The value of the integrall of cos(x) between 0 and pi/2 for 1000 points is: ", integrate(0, 0.5 * (np.pi), 1000, np.cos)
 print
 
-print "The value of the intergral of cos(x) between 0 and pi/2 for 1000000 points is: ", intergrate(x6)
+print "The value of the integrall of cos(x) between 0 and pi/2 for 1000000 points is: ", integrate(0, 0.5 * (np.pi), 1000000, np.cos)
+print
+
 print
 
 print "As is evident, a higher number of points correlates to a higher accuracy in the calculation "
@@ -79,7 +97,8 @@ print
 print
 
 # Taking all even numbers, skipping the first and last ones
-
+print
+print
 print a[4:-2:2]
 print
 print
