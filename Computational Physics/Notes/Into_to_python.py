6459e596dfa96f32240f9c513ffde0a7d87b6e8f
Kreason Aaron Naidoo <kreasonaaron@me.com>
2016-04-29 21:53:34 +0200

    Started the N body project Created two classes, a main class for the project to work in and a class of point masses with some basic information about the masses. This second class is not needed but does simplify the storing of information for the point masses.

---
+++ Computational Physics/Notes/Into_to_python.py
@@ -0,0 +1,157 @@
+# this is my first project file.
+
+import numpy as np
+
+import Gauss_Example as ga
+
+from  matplotlib import pyplot as plt
+
+print "hello world"
+
+''' python block comment
+
+# integers.
+
+x = 5
+
+print x
+
+print type(x)
+
+print""
+
+#floats.
+
+y = 0.3
+
+print y
+
+print type(y)
+
+print""
+
+#addition of different types.
+
+print x+y
+
+print type(x+y)
+
+print""
+
+
+#lists (like java arrays, start at 0).
+
+z = [x,y,(x+y)]
+
+print z[1] #prints the secound entry in the list.
+
+# -1 is the index of the final point in the array, -2 is the 2nd last ect
+# len(z) returns the length of the array
+
+print len(z)
+
+print z[-1]
+print z[-2]
+
+
+#tuples
+#like lists but you cant change them, like FINAL lists / arrays.
+
+# For loops
+
+aa = range(4,21) #will give a list of numbers from 0 to 9
+
+# syntax for x in aa:
+    #commands
+
+# new style for loops
+for i in aa:
+    print i
+
+
+#for a more traditional for loop (java / c++), I prefer this
+
+for k in range(len(aa)):
+    print k , aa[k]
+
+# NB the print command leaves a line (like println in java)
+# It's easier to use double quotes for strings (for the cases of words with apostropes)
+
+# x**3 is x to the power 3
+
+# maths in python using imported libraries
+
+import math
+
+for z in range(10):
+    print math.sin(z)
+
+
+# we can also import only the packages of the library that we need
+
+from math import sin
+
+for z in range(10):
+    print math.sin(z)
+
+# if we use from math import *, we don't need to have math. in front of sin
+
+#numpy example
+import numpy as np
+
+a = np.array([1,2,3])
+
+for d in range(len(a)):
+    print a[d]
+
+
+#matplotlib example
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(0, 30, 256, endpoint=True) #determins the jumps in X, giving us a smooth curve
+plt.plot((0, 30), (0, 0), '-k') #this is just a y axis
+
+plt.plot(X, np.sin(X))
+plt.plot(X, np.cos(X))
+
+plt.ylabel("Y Values")
+plt.xlabel("X Values")
+
+#this will show our numpy graph
+plt.show()
+
+
+# if statments
+
+#for x in range (11):
+ #   if (x%2 == 0):
+  #      print x
+
+# this would print only the even numbers from 0 to 10
+
+
+# numpy arrays
+
+import numpy as np
+
+x = np.arange(10) #an array with numbers 0 to 9
+
+
+
+# Functions in python (methods)
+# Gaussian std gen form  ae^(-(x-x0)^2/2sigma^2)
+
+
+X = np.arange(-5,5,0.0001)
+plt.plot(X, ga.gauss(X, -1, 1, 1))
+plt.plot(X, ga.gauss(X,amp = 1, x0 = 1,sigma = 1))
+plt.plot(X, ga.gauss(X))
+
+plt.show()
+
+'''
+
+# Simpsons Rule
+
+#integral = dx*(1/6 Y0 + 2/3 Yodd +1/3 Yeven + 1/6 Ylast)
---
+++ Computational Physics/Tutorials/Tutorial 2.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+# Question 1
+
+print
+print "Question 1 has no output"
+print
+print
+
+'''used in old method
+x1 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/10) # x is a range of numbers from 0 to half pi, 10 points
+x2 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/30) # x is a range of numbers from 0 to half pi, 30 points
+x3 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/100) # x is a range of numbers from 0 to half pi, 100 points
+x4 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/300) # x is a range of numbers from 0 to half pi, 300 points
+x5 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/1000) # x is a range of numbers from 0 to half pi, 1000 points
+x6 = np.arange(0,0.5*(np.pi),(0.5*(np.pi))/1000000) # x is a range of numbers from 0 to half pi, 1000000 points
+
+
+# Question 2
+
+def integrate_old (ran):
+
+    width = ran[1] - ran[2]
+
+    sum = 0
+
+    for i in range(len(ran)):
+        sum = sum + (np.cos(ran[i])*width)
+
+    return sum
+
+'''
+
+def integrate(low, high, points, function):
+
+    step = (high - low) / points
+
+    x = np.arange(low, high, step)
+
+
+    sum = 0
+
+    for i in range(len(x)):
+        sum = sum + function(x[i]) * step
+
+    return sum
+
+
+
+
+print "Question 2"
+print
+print
+
+print "The actual value of the integral of cos(x) between 0 and pi/2 is 1"
+print
+print
+
+print "The value of the integral of cos(x) between 0 and pi/2 for 10 points is: ", integrate(0, 0.5 * (np.pi), 10, np.cos)
+print
+
+print "The value of the integral of cos(x) between 0 and pi/2 for 30 points is: ", integrate(0, 0.5 * (np.pi), 30, np.cos)
+print
+
+print "The value of the integrall of cos(x) between 0 and pi/2 for 100 points is: ", integrate(0, 0.5 * (np.pi), 100, np.cos)
+print
+
+print "The value of the integrall of cos(x) between 0 and pi/2 for 300 points is: ", integrate(0, 0.5 * (np.pi), 300, np.cos)
+print
+
+print "The value of the integrall of cos(x) between 0 and pi/2 for 1000 points is: ", integrate(0, 0.5 * (np.pi), 1000, np.cos)
+print
+
+print "The value of the integrall of cos(x) between 0 and pi/2 for 1000000 points is: ", integrate(0, 0.5 * (np.pi), 1000000, np.cos)
+print
+
+print
+
+print "As is evident, a higher number of points correlates to a higher accuracy in the calculation "
+
+print
+print
+print
+
+print "Question 3"
+
+# Question 3
+# We wil assume an ordered array starting at 0
+
+# Taking all odd numbers from an array
+
+a = np.arange(101)
+print
+print
+print a[1:-1:2] # -1 denotes the end of the array,
+print
+print
+
+# Taking all even numbers, skipping the first and last ones
+print
+print
+print a[4:-2:2]
+print
+print
+
+
+
