770d6d0945c2fd29b4e1835cbeb09660cc6ad26a
Kreason Aaron Naidoo <kreasonaaron@me.com>
2016-06-20 13:39:11 +0200

    updates particle class

--- N - Body Project/NBody.py
+++ N - Body Project/NBody.py
@@ -1,5 +1,5 @@
 
-import PointMass as PM
+import particle as PM
 
 
 class N_Body:
@@ -7,45 +7,24 @@ class N_Body:
 
     def __init__(self):
 
-        self.options = {} #dictionary of constants
 
-        self.options["N"] = 0
-        self.options["G"] = 6.67300 * 10 ** -11
+        Number_of_particles = 100000  #Number of particles
 
-        self.part = [] #list of all particals
+        G = 6.67300 * 10 ** -11
+
+        Grid_Size = 100; #The grid is this number on each side
+
+
+
 
 
     def add(self, m, x, y):
 
-        self.part.append(PM.PointMass(m, x, y)) #could be written to accept point mass objects instead of raw data on the points
-        self.options["N"] = self.options["N"] + 1
+        self.Number_of_particles = self.Number_of_particles + 1
 
 
 
 
-    def pot_energy(self, a, b): # potential energy between interacting particles
 
-        Rba =  (((b.x - a.x)**2) + ((b.y - a.y)**2))**0.5 #distance formula
-
-        if Rba == 0:
-
-            return 0
-
-        else:
-
-            return (self.options["G"] * a.m * b.m) / Rba
-
-
-
-    def tot_pot_energy(self):
-
-        sum2 = 0
-
-        for m in range(0,self.options["N"]):
-            for n in range(m+1, self.options["N"]):
-
-                    sum2 = sum2 + self.pot_energy(self.part[m], self.part[n])
-
-        return sum2
 
 
---
+++ N - Body Project/particle.py
@@ -0,0 +1,49 @@
+
+
+import numpy as np
+
+
+class PointMass:
+
+    def __init__(self, m, x, y,):
+
+        self.mass = m
+
+        self.position_x = x # x position
+        self.position_y = y # y position
+
+        self.force_x = 0 # x force
+        self.force_y = 0 # y force
+
+        self.velocity_x = 0 # x velocity
+        self.velocity_y = 0 # y velocity
+
+        self.acceleration_x = 0 # x acceleration
+        self.acceleration_y = 0 # y acceleration
+
+        # self.potential_x = 0
+        # self.potential_y = 0
+
+
+
+    def solve_force(self, ux_left, ux_right, uy_left, uy_right):
+
+
+
+        self.force_x = -1 * (ux_left - ux_right)/2
+        self.force_y = -1 * (uy_left - uy_right) / 2
+
+        self.acceleration_x = self.force_x / self.mass
+        self.acceleration_y = self.force_y / self.mass
+
+
+    def solve_velocity(self, dt):
+
+        self.velocity_x = self.velocity_x + self.force_x * dt
+        self.velocity_y = self.velocity_y + self.force_y * dt
+
+    def solve_position(self, dt):
+
+        self.position_x = self.position_x + self.velocity_x * dt
+        self.position_y = self.position_y + self.velocity_y * dt
+
